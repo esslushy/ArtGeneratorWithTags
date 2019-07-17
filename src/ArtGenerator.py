c870d0e8102090fe472e35862225fa4f0c8672be
Evan Schlesinger <esslushy@gmail.com>
2019-07-17 10:32:56 -0500

    finished building training loop

--- src/ArtGenerator.py
+++ src/ArtGenerator.py
@@ -15,22 +15,21 @@ parser.add_argument('--images', type=str, help='The location to the folder conta
 parser.add_argument('--tags', type=str, help='The location to the .npy containing the labels', default='./dataset/tags.npy')
 parser.add_argument('--tensorboard', type=str, help='The location to save the .info file for Tensorboard', default='./info')
 parser.add_argument('--save_model', type=str, help='The location to save the model files to during training and at the end', default='./model')
-parser.add_argument('--settings', type=str, help='Path to the json files with the settings', required=True)
+parser.add_argument('--settings', type=str, help='Path to the json files with the settings. Use this instead of passing arguments to make it easier to rerun tests with the same values.', required=True)
 arguments = parser.parse_args()
 
-#Gather settings from json
+# Gather settings from json
 with open(arguments.settings, 'r') as f:
     settings = json.load(f)
 
-#Set global variables
-settings['noiseLength'] = 100
+# Set global variables
 gloablStep = 0
 
-#Standardize randomness
+# Standardize randomness
 tf.random.set_seed(7)
 np.random.seed(7)
 
-#Set prefetch buffer for dataloading
+# Set prefetch buffer for dataloading
 AUTOTUNE = tf.data.experimental.AUTOTUNE
 
 # Build dataset
@@ -43,9 +42,9 @@ def loadAndPreprocessImage(path):
     image = image / (255.0/2.0)
     return image
 
-#Get tags and load into a tensorflow dataset
+# Get tags and load into a tensorflow dataset
 tagDataset = tf.data.Dataset.from_tensor_slices(tf.cast(np.load(settings['tags']), tf.float32))#float 32 used for compatible typing
-#Get all locations of pictures and load into the dataset
+# Get all locations of pictures and load into the dataset
 imageRoot = pathlib.Path(settings['images'])#lets use get images from the folder
 imageDataset = tf.data.Dataset.from_tensor_slices([str(path) for path in imageRoot.iterdir()])#Gets all image paths
 imageDataset = imageDataset.map(loadAndPreprocessImage, num_parallel_calls=AUTOTUNE)# Places images into datast
@@ -56,12 +55,12 @@ dataset = dataset.shuffle(5000000)
 dataset = dataset.batch(settings['batchSize'])
 dataset = dataset.prefetch(buffer_size=AUTOTUNE)
 
-#Build models
+# Build models
 with tf.device('/GPU:0'): # Gpu used for training
     generator = buildGenerator(training=True)
     discriminator = buildDiscriminator(training=True)
 
-# Losses
+# Loss Object
 bceLoss = keras.losses.BinaryCrossentropy()
 
 #Optimizers
@@ -72,15 +71,79 @@ discriminatorOptimizer = keras.optimizers.Adam(settings['learningRate'])
 discriminatorRealImagesAccuracy = keras.metrics.BinaryAccuracy()
 discriminatorFakeImagesAccuracy = keras.metrics.BinaryAccuracy()
 
+# Loss functions
+@tf.function
+def generatorLoss(fakePredictions):
+    # Ones like because the label for real images is 1, and the generator wants to make its images as realistic as possible
+    return bceLoss(fakePredictions, tf.ones_like(fakePredictions))# Should be a shape of (batchSize, 1)
+
+@tf.function
+def discriminatorLoss(realPredictions, fakePredictions):
+    # Ones like because the label for real images is 1, and the discriminator wants to approach that with its predictions on the real images
+    discriminatorRealLoss = bceLoss(realPredictions, tf.ones_like(realPredictions))# Should be a shape of (batchSize, 1).
+    # Zeros like because the label for fake images is 0, and the discriminator wants to approach that with its predictions on the generators images
+    discriminatorFakeLoss = bceLoss(fakePredictions, tf.zeros_like(fakePredictions))# Should be a shape of (batchSize, 1).
+    # Add losses together to pass to optimizer
+    discriminatorTotalLoss = discriminatorRealLoss + discriminatorFakeLoss
+    return discriminatorRealLoss, discriminatorFakeLoss, discriminatorTotalLoss
+
 # Train step
 @tf.function
 def trainStep(images, labels):
-    noise = tf.random.normal((images.shape[0], 100))#Makes a random noise distribution of (Batch Size, 100)
+    noise = tf.random.normal((images.shape[0], 100))#Makes a random noise distribution of (batchSize, 100)
+    with tf.GradientTape() as tape:
+        # Build fake images
+        fakeImages = generator(noise, labels)
+        # Get discriminator predictions
+        realPredictions = discriminator(images, labels)
+        fakePredictions = discriminator(fakeImages, labels)
+        # Calculate losses
+        genLoss = generatorLoss(fakePredictions)
+        discRealLoss, discFakeLoss, discTotalLoss = discriminatorLoss(realPredictions, fakePredictions)
+
+    # Collect Gradients
+    generatorGradients = tape.gradient(genLoss, generator.trainable_variables)
+    discriminatorGradients = tape.gradient(discTotalLoss, discriminator.trainable_variables)
+
+    # Run Optimizers
+    generatorOptimizer(zip(generatorGradients, generator.trainable_variables))
+    discriminatorOptimizer(zip(discriminatorGradients, discriminator.trainable_variables))
+
+    # Accumalate Metrics
+    discriminatorRealImagesAccuracy.update_state(tf.ones_like(realPredictions), realPredictions)
+    discriminatorFakeImagesAccuracy.update_state(tf.zeros_like(fakePredictions), fakePredictions)
+
+    # Log to tensorboard
+    tf.summary.scalar('Discriminator_Real_Images_Loss', tf.reduce_mean(discRealLoss), step=gloablStep)
+    tf.summary.scalar('Discriminator_Fake_Images_Loss', tf.reduce_mean(discFakeLoss), step=gloablStep)
+    tf.summary.scalar('Discriminator_Real_Images_Accuracy', discriminatorRealImagesAccuracy.result().numpy(), step=gloablStep)
+    tf.summary.scalar('Discriminator_Fake_Images_Accuracy', discriminatorFakeImagesAccuracy.result().numpy(), step=gloablStep)
+    tf.summary.scalar('Generator_Loss', tf.reduce_mean(genLoss), step=gloablStep)
+    tf.summary.image('Generated_Images', fakeImages, max_outputs=8, step=gloablStep)
+
+# Checkpoint Model
+checkpoint = tf.train.Checkpoint(generatorOptimizer=generatorOptimizer, discriminatorOptimizer=discriminatorOptimizer,
+                                generator=generator, discriminator=discriminator)
+manager = tf.train.CheckpointManager(checkpoint, directory=settings['saveModel'] + 'checkpoints', max_to_keep=3, checkpoint_name='ckpt_epoch')#Keep only last 3 checkpoints of model
+
+# Summary Writer
+writer = tf.summary.create_file_writer(settings['tensorboardLocation'])
 
 # Training
-for epoch in range(settings['epochs']):
-    for images, labels in dataset:
-        trainStep(images, labels)
-        # Reset metrics so the accumalate fresh each step
+with writer.as_default(): # All summaries made during training will be saved to this writer
+    for epoch in range(settings['epochs']):
+        for images, labels in dataset:
+            # Train model and update tensorboard
+            trainStep(images, labels)
+            # Increment global step for logging to tensorboard
+            gloablStep+=1
+
+        # Checkpoint model each epoch
+        manager.save(checkpoint_number=epoch)
+        # Reset metrics so that they accumalate per epoch instead of over the entire training period
         discriminatorRealImagesAccuracy.reset_states()
         discriminatorFakeImagesAccuracy.reset_states()
+        
+# Save Final trained models in keras model format for easy reuse
+tf.saved_model.save(generator, settings['saveModel'] + 'generator')
+tf.saved_model.save(discriminator, settings['saveModel'] + 'discriminator')
